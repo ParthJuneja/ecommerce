f9a81086e7827e2cc8d28afb812ce9dcba15fe95
parth <admin@test.com>
2023-03-09 00:40:42 +0530

    added products

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
     </div>
 
     <div className="products-container">
-      {/* {products?.map((product) => <Product key={product._id} product={product} />)} */}
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
     {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
