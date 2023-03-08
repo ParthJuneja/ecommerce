e3f4a4995c1bf2195dbb1e0d902555d5955673bd
parth <admin@test.com>
2023-03-09 00:17:43 +0530

    banner created

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-// import { client } from '../lib/client';
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]}  /> */}
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
@@ -19,16 +19,16 @@ const Home = ({ products, bannerData }) => (
   </div>
 );
 
-// export const getServerSideProps = async () => {
-//   const query = '*[_type == "product"]';
-//   const products = await client.fetch(query);
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
 
-//   const bannerQuery = '*[_type == "banner"]';
-//   const bannerData = await client.fetch(bannerQuery);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
-//   return {
-//     props: { products, bannerData }
-//   }
-// }
+  return {
+    props: { products, bannerData }
+  }
+}
 
 export default Home;
