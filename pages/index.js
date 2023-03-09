150ca4f631fdd090f359e9e4370af599915611de
parth <admin@test.com>
2023-03-09 20:22:47 +0530

    Layout and components done

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,14 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <p> Choose from wide variety of products </p>
     </div>
 
     <div className="products-container">
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
-    {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 
