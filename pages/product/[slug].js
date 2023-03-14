d08937a7a3a763cfe150049d062f128136a871d7
parth <admin@test.com>
2023-03-14 16:03:00 +0530

    slug not working

---
+++ pages/product/[slug].js
@@ -0,0 +1,58 @@
+import { client, urlFor } from "../../lib/client";
+import React from "react";
+
+
+const ProductDetails = ({ product, products }) => {
+    // const { image, name, details, price } = product;
+
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div className="image-container">
+          {console.log(product)}
+          {/* <img src={urlFor(image && image[0])} /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+      slug {
+        current
+      }
+    }
+    `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]';
+  
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+  
+  
+    return {
+      props: {product, products },
+    };
+  };
+
+
+
+
+export default ProductDetails;
